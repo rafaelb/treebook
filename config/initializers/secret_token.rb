8eb9ad904305a7597a81c7e28f4581a38fd9f2b0
Rafael Borochov <rafael.borochov@gmail.com>
2013-08-11 11:01:43 +0300

    moved to rail 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '64af05df419234670b0258fdd6efbe4547847dbee26381ea68ed2fdab7358c1dedc3763e1b7ad3b012ebf7a7b0ead5748212334865b14873cf2a2db4d92e96e8'
