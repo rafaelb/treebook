06a74b00a9f2c5a85d8e474d749a9cc8c40eae04
Rafael Borochov <rafael.borochov@gmail.com>
2013-08-18 22:35:16 +0300

    Added test for custom routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+	def flash_class(type)
+		case type
+		when :alert
+			"alert-danger"
+		when :notice
+			"alert-success"
+		else
+			""
+			
+			
+		end
+		
+	end
+
 end
